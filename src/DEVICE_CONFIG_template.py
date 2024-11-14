cb96b111a3a9e95ab6f9420d17a6ce110ab8843b
learnbanjo <48936531+learnbanjo@users.noreply.github.com>
2024-11-13 16:45:32 -0800

    coorect error in the DEVICE_CONFIG_template.py

--- src/DEVICE_CONFIG_template.py
+++ src/DEVICE_CONFIG_template.py
@@ -2,6 +2,6 @@ DEVICE_NAME = "device host name" # Descriptive name for the device
 DEVICE_TYPE = "Flood Sensor" # Sensor Type. "Flood Sensor", "Pressure Sensor"
 DEVICE_KEY = "" #device private key for future use cases
 SSID = "my wifi hotspot name"
-PASSWORD = "wifi password". #Use "" for no password
+PASSWORD = "wifi password" #Use "" for no password
 ANALOG_SENSOR_PIN = 0 # ESP8266 ADC pin is 0. Set to "" to disable
 DIGITAL_SENSOR_PIN = 14 # ESP8266 GPIO pin is 14. Set to "" to disable
