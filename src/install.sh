9e2859e5d671f944376a5e0694e84b0e875350eb
learnbanjo <48936531+learnbanjo@users.noreply.github.com>
2024-11-13 16:16:16 -0800

    Installation Script

---
+++ src/install.sh
@@ -0,0 +1,5 @@
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put DEVICE_CONFIG.py
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put main.py
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put ota.py
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put utils.py
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put boot.py
