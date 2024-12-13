6ea44254874ccf29524e56372f690f5c5ff1a487
learnbanjo <48936531+learnbanjo@users.noreply.github.com>
2024-12-12 16:36:03 -0800

    remove install.sh

--- src/install.sh
+++
@@ -1,5 +0,0 @@
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put DEVICE_CONFIG.py
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put main.py
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put ota.py
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put utils.py
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put boot.py
