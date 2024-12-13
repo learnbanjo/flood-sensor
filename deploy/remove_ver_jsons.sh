a1a0d7de9fd23c488996d0a50d508c3da1a9a69f
learnbanjo <48936531+learnbanjo@users.noreply.github.com>
2024-12-12 16:49:37 -0800

    fix bug

--- deploy/remove_ver_jsons.sh
+++ deploy/remove_ver_jsons.sh
@@ -2,11 +2,11 @@ echo removing main.py_ver.json
 sudo ampy -p /dev/tty.usbserial-1410 -b 115200 rm main.py_ver.json
 echo done
 echo removing ota.py_ver.json
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put ota.py_ver.json
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 rm ota.py_ver.json
 echo done
 echo removing utils.py_ver.json
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put utils.py_ver.json
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 rm utils.py_ver.json
 echo done
 echo removing boot.py_ver.json
-sudo ampy -p /dev/tty.usbserial-1410 -b 115200 put boot.py_ver.json
+sudo ampy -p /dev/tty.usbserial-1410 -b 115200 rm boot.py_ver.json
 echo done
