73af40d537b998c1a5a08a19fa5b87279a6c86cf
learnbanjo <48936531+learnbanjo@users.noreply.github.com>
2024-12-12 16:10:22 -0800

    Update DEVICE_CONFIG_template.py to support MQTT

--- src/DEVICE_CONFIG_template.py
+++ src/DEVICE_CONFIG_template.py
@@ -4,4 +4,9 @@ DEVICE_KEY = "" #device private key for future use cases
 SSID = "my wifi hotspot name"
 PASSWORD = "wifi password" #Use "" for no password
 ANALOG_SENSOR_PIN = 0 # ESP8266 ADC pin is 0. Set to "" to disable
-DIGITAL_SENSOR_PIN = 14 # ESP8266 GPIO pin is 14. Set to "" to disable
+DIGITAL_SENSOR_PIN = 5 # ESP8266 GPIO pin is D1 (GPIO5). Set to "" to disable. See https://electropeak.com/learn/esp8266-pinout-reference-how-to-use-esp8266-gpio-pins/
+
+MQTT_PUBLISH_INTERVAL = 30
+mqtt_broker_address = "127.0.0.1"
+mqtt_broker_port = 1883
+mqtt_keep_alive_time = 360
